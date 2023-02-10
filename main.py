45a5878092a1e845d67ebf75caef149d182eeaac
sherin <skaryil@equinix.com>
2023-02-10 14:03:35 +0530

    added decimal to binary function

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
+
+def dec_to_binary(n):
+    binaryNum = [0] * n
+    i = 0
+    while (n > 0):
+        binaryNum[i] = n % 2
+        n = int(n / 2)
+        i += 1
+
+    binaryNum.reverse()
+
+    return binaryNum
+
 def binary_to_decimal(number):
     decimal = 0
     
