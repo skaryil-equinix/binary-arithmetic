821ec25e5a204062f61b75c6aed1fa7fb372ef8d
Aryan Arora <ararora@equinix.com>
2023-02-10 14:21:55 +0530

    Added add function

--- main.py
+++ main.py
@@ -1,15 +1,11 @@
 
 def dec_to_binary(n):
-    binaryNum = [0] * n
-    i = 0
+    binaryNum = ""
     while (n > 0):
-        binaryNum[i] = n % 2
+        binaryNum = binaryNum + str(n%2)
         n = int(n / 2)
-        i += 1
 
-    binaryNum.reverse()
-
-    return binaryNum
+    return "".join(binaryNum[::-1])
 
 def binary_to_decimal(number):
     decimal = 0
@@ -18,3 +14,40 @@ def binary_to_decimal(number):
         decimal = decimal*2 + int(digit)
     
     return decimal
+
+def add_binary(n1, n2):
+    n1 = dec_to_binary(n1)
+    n2 = dec_to_binary(n2)
+
+    max_len = max(len(n1), len(n2))
+
+    n1 = n1.zfill(max_len)
+    n2 = n2.zfill(max_len)
+
+    res = ""
+    carry = 0
+
+    for i in range(max_len - 1, -1, -1):
+        re = carry
+        if n1[i] == '1':
+            re = re+1
+        else:
+           re = re+0
+        if n2[i] == '1':
+          re = re+1
+        else:
+            re = re+0
+        if re%2==1:
+            res = '1' + res
+        else:
+            res = '0' + res
+        if re<2:
+            carry = 0
+        else:
+            carry = 1
+
+    if carry!=0:
+        res = '1' + res
+
+    print(res)
+    print(binary_to_decimal(res))
