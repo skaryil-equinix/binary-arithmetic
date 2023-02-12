baf660999b9737dedd1a809c70cb38c4890bec98
sherin <skaryil@equinix.com>
2023-02-12 17:26:34 +0530

    added mul binary function for 32 bin binary array as input

--- main.py
+++ main.py
@@ -108,85 +108,18 @@ def sub_binary(n1, n2):
 
     return tot
 
-def sub_decimal(num1, num2):
+ 
+def mul_binary(num1, num2):
 
-    n1 = num1
-    n2 = num2
+    tot = [0 for i in range(0,32)]
 
-    one_s=''
-    #take 1s of num2
-    for i in range(len(n2)):
-        if(n2[i]=='0'):
-            one_s+='1'
-        else:
-            one_s+='0'
-
-    two_s=''
-    flag=1
-    #take 2s of num2
-    for i in range(len(one_s)-1, -1, -1):
-        if flag==1:
-            if(one_s[i]=='0'):
-                two_s = '1'+two_s
-                flag=0
-            else:
-                two_s = '0'+two_s
-        else:
-            two_s = one_s[i]+two_s
-
-    n1 = n1
-    n2 = two_s
-
-    max_len = max(len(n1), len(n2))
-
-    n1 = n1.zfill(max_len)
-    n2 = n2.zfill(max_len)
-
-    res = ""
-    carry = 0
-
-    for i in range(max_len - 1, -1, -1):
-        re = carry
-        if n1[i] == '1':
-            re = re+1
-        else:
-           re = re+0
-        if n2[i] == '1':
-          re = re+1
-        else:
-            re = re+0
-        if re%2==1:
-            res = '1' + res
-        else:
-            res = '0' + res
-        if re<2:
-            carry = 0
-        else:
-            carry = 1
-
-    if carry!=0:
-        res = '1' + res
-
-    res = res[len(res)-32:]
-
-    return res
-
-
-def mul_decimal(num1, num2):
-
-    arr = []
-    offset = '0'
-    for c in reversed(num2):
-
-        # num1 = num1[len(num1)-32:]
-        if c == '1':
-            arr.append(num1)
+    for i in range(31, -1, -1):
         
-        num1 = num1+offset
-
-    tot='0'
-    for ele in arr:
-        tot = add_decimal(tot, ele)
+        if num2[i] == 1:
+            tot = add_binary(tot, num1)
+            
+        num1.pop(0)
+        num1.append(0)
     
     return tot
 
